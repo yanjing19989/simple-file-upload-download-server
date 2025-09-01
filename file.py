4765653c0e2666175b536d1e8214268b16e3de10
yjc <yanjing19981999@126.com>
2025-09-01 14:07:58 +0800

    显示监听ip+端口

--- file.py
+++ file.py
@@ -215,7 +215,8 @@ class UploadHTTPRequestHandler(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=UploadHTTPRequestHandler, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f"Serving on port {port}")
+    import socket
+    print(f"Serving on {socket.gethostbyname(socket.gethostname())}:{port}")
     httpd.serve_forever()
 
 if __name__ == '__main__':
